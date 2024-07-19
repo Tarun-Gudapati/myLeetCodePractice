d9c673dd4f056d436c06f3345aac7fc0965277c6
Tarun Gudapati <94289102+Tarun-Gudapati@users.noreply.github.com>
2024-07-19 21:06:03 +0530

    Create understandingStack.java

---
+++ understandingStack.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class UnderstandingStackDs {
+    public static void main(String[] args) {
+        Stack<String> st = new Stack<>();
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 3; i++)
+            st.push(getInput(sc));
+        System.out.println(st);
+        System.out.println(st.pop());
+        System.out.println(st);
+
+    }
+
+    public static String getInput(Scanner sc) {
+        String s = sc.next();
+        return s;
+    }
+}
