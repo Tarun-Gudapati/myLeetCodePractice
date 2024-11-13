ce8c268e67c098fa95d0be49bbbe30c8a3367931
Tarun Gudapati <94289102+Tarun-Gudapati@users.noreply.github.com>
2024-11-13 22:48:10 +0530

    Update CreatingMessageBox.java

--- CreatingMessageBox.java
+++ CreatingMessageBox.java
@@ -8,7 +8,7 @@ public class CreatingAMessageBox {
         String str = JOptionPane.showInputDialog("enter your name");
         JOptionPane.showMessageDialog(null, str);
         int age = Integer.parseInt(JOptionPane.showInputDialog("enter your age"));
-        JOptionPane.showMessageDialog(null, str + " is your age " + age);
+        JOptionPane.showMessageDialog(null, str + " is your age " + age);;
 
     }
 }
